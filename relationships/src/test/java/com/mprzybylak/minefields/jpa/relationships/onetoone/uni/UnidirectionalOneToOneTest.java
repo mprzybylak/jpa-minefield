eefe64d87a4e4137299fa5f84b6404f89d117b58
Michal Przybylak <michal.przybylak@gmail.com>
2014-04-30 16:49:13 +0200

    format

--- relationships/src/test/java/com/mprzybylak/minefields/jpa/relationships/onetoone/uni/UnidirectionalOneToOneTest.java
+++ relationships/src/test/java/com/mprzybylak/minefields/jpa/relationships/onetoone/uni/UnidirectionalOneToOneTest.java
@@ -23,37 +23,38 @@ public class UnidirectionalOneToOneTest {
 		emf = Persistence.createEntityManagerFactory("pu");
 		em = emf.createEntityManager();
 	}
-	
+
 	@Test
 	public void shouldCreateRelation() {
-		
+
 		// given
 		FootballPlayer footballPlayer = new FootballPlayer(FOOTBALL_PLAYER_ID);
 		Ball ball = new Ball(BALL_ID);
-		
+
 		// when
 		em.getTransaction().begin();
 		footballPlayer.setBall(ball);
 		em.persist(ball);
 		em.persist(footballPlayer);
 		em.getTransaction().commit();
-		
+
 		// then
-		TypedQuery<FootballPlayer> query = em.createQuery("SELECT e FROM FootballPlayer e", FootballPlayer.class);
-		
+		TypedQuery<FootballPlayer> query = em.createQuery(
+				"SELECT e FROM FootballPlayer e", FootballPlayer.class);
+
 		FootballPlayer databasePlayer = query.getSingleResult();
 		assertThat(databasePlayer).isNotNull();
 		assertThat(databasePlayer.getId()).isEqualTo(FOOTBALL_PLAYER_ID);
-		
+
 		Ball databaseBall = databasePlayer.getBall();
 		assertThat(databaseBall).isNotNull();
-		assertThat(databaseBall.getId()).isEqualTo(BALL_ID); 
+		assertThat(databaseBall.getId()).isEqualTo(BALL_ID);
 	}
-	
+
 	@After
 	public void tearDownOnce() {
 		em.close();
 		emf.close();
 	}
-	
+
 }
